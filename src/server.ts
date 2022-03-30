41a4caa8c0939e6dc2bd57eae71d5256df879ed8
Augusto Almeida <augustalmeid@gmail.com>
2022-03-30 09:10:45 -0300

    Correção da configuração de eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({message: 'Ola mundo TS'});
-})
+app.get("/", (req, res) => {
+  return res.json({ message: "Ola mundo TS" });
+});
 
 app.listen(3000);
